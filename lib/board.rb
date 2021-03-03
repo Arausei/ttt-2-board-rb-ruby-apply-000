3e2ae25c7bba93b8368d06aa8d9ade3b2935535c
Arausei Johnson <arausei@yahoo.com>
2021-03-03 16:33:32 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,18 @@
 # Define the variable board below
 board = Array[" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def see_me_ten_times
+  pharse = "see me"
+puts pharse
+puts pharse
+puts pharse
+puts pharse
+puts pharse
+puts pharse
+puts pharse
+puts pharse
+puts pharse
+puts pharse
+end
+
+" 
